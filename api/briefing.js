cad28ea3eaa2f37201a2d7ccaca43be4b8f300a7
ClaraDina <87046355+claradina@users.noreply.github.com>
2026-05-04 14:29:51 -0300

    chore: trigger redeploy to load env variables

--- api/briefing.js
+++ api/briefing.js
@@ -7,6 +7,7 @@ const HUGGING_FACE_MODEL = 'mistralai/Mistral-7B-Instruct-v0.3';
 function buildPrompt(account) {
   return `<s>[INST] You are a Customer Success Manager in the solar energy industry preparing for a client call. Generate a concise pre-call briefing (max 180 words) in plain text with no markdown headers.
 
+
 Account: ${account.name}
 Country: ${account.country}
 Segment: ${account.segment}
